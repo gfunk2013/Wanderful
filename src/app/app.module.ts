f001b987fdfe87f5d0cf1c6f4b3b96c60269bbc4
Gerhardt <13gfunk@gmail.com>
2018-07-09 19:18:56 -0600

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '../../node_modules/@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { GlobalVarService } from './shared/global-var.service';
 import { MatchHeightDirective } from './shared/match-height.directive';
@@ -15,6 +16,7 @@ import { FooterComponent } from './footer/footer.component';
 import { WeatherListComponent } from './weather-list/weather-list.component';
 import { VenueListComponent } from './venue-list/venue-list.component';
 import { ResizeIconPipe } from './weather-list/resize-icon.pipe';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 @NgModule({
@@ -28,12 +30,17 @@ import { ResizeIconPipe } from './weather-list/resize-icon.pipe';
     ValidUvPipe,
     CityNamePipe,
     ResizeIconPipe,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'weather', component: WeatherListComponent},
+      { path: 'venues', component: VenueListComponent},
+    ])
   ],
   providers: [
     GlobalVarService,
